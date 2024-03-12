fe726fa019ffe998c68cf5949b73bd5ed21f325e
Saullo Almeida <saullorhamon@gmail.com>
2024-03-12 20:26:10 -0300

    create project, model note, factory, seed and routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
+use App\Http\Controllers\NoteController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+Route::get('/note', [NoteController::class, 'index'])->name('note.index');
+Route::get('/note/create',[NoteController::class,'create'])->name('note.create');
+Route::post('/note', [NoteController::class,'store'])->name('note.store');
+Route::get('/note/{note}', [NoteController::class, 'show'])->name('note.show');
+Route::get('/note/{note}/edit', [NoteController::class, 'edit'])->name('note.edit');
+Route::put('/note/{note}', [NoteController::class, 'update'])->name('note.update');
+Route::delete('/note/{note}', [NoteController::class, 'destroy'])->name('note.destroy');
